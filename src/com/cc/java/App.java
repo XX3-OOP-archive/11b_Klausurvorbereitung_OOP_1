e305e565e2480c595c03e743aa7b3eff0eac7f5c
Peter <schmidt.multimedia@gmail.com>
2021-12-07 10:31:48 +0100

    update Themen

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,7 +4,15 @@ public class App {
     
     public static void main(String[] args) {
 
-    
+    // test --> Referenzvariable / RAM-Adresse des Objekts/Instanz (object/instance)
+    // Test --> Referenztyp (reference type)
+    // Object <--> Instanz
+
+    Test test  = new Test();
+    System.out.println("Referenzvariable: " + test);
+    test.showAddress();
+
+
     }
 
 }
